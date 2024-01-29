ff28d5dd84fe053a4f33f1f3dc24a35bc515623e
agadzik <andrew.gadzik@vercel.com>
2024-01-29 18:30:22 -0500

    Initial commit

---
+++ test/cdk-palworld.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPalworld from '../lib/cdk-palworld-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-palworld-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPalworld.CdkPalworldStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
